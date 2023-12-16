3f08690835cf7c9c004d0aa0e822bc22cfa44e7d
MorganNjoroge <zeejaymorgan@morgan.com>
2023-12-16 11:28:25 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideddistance){
+  if(rideddistance <=400){
+    return "This one is on me!";
+  
+ 
+  }else if(rideddistance>=2000 && rideddistance<2500){
+     return "I will gladly take your thirty bucks.";
+
+  }else if(rideddistance >2500){
+    return 'No can do.'
+
+
+
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+  
+
+
+function ternaryCheckCity(city){
+  // if (city==='NYC'){
+  //   return "OK, sounds good."
+  // }else {
+  //   return 'No go.'
+
+
+  // }
+return city ==="NYC"?"Ok, sounds good.":"No go."
+  
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+
+
+
+  }
+
   // Write your code here!
 }
